c502265fe3122274b3c706505a91d24c1c8b5b81
KleinerMeteor <KleinerMeteor@web.de>
2019-08-25 16:32:59 +0200

    Set up simple electron app with a h1 tag

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const electron = require("electron");
 const app = electron.app;
 const browserWindow = electron.BrowserWindow;
+const url = require("url");
+const path = require("path");
 
-let win = new browserWindow({
+let win;
+
+function createWindow() {
+    win = new browserWindow();
     
-}); 
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
 
+    win.on('close', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
